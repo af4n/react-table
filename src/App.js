e07cc77630eee9021af50684e5d03d5060ea7ffb
af4n <aleksey.afanaskin@gmail.com>
2020-12-09 23:37:25 +0200

    sort table and discription person was added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import _ from 'lodash'
 import Loader from "./components/Loader/Loader";
 import Table from "./components/Table/Table";
+import DetailRowView from "./components/DetailRowView/DetailRowView";
 
 class App extends React.Component{
 
   state = {
     isLoading: true,
-    data: []
+    data: [],
+    sort: 'asc', // desk
+    sortField: 'id',
+    row: null
+
   }
 
   async componentDidMount() {
@@ -15,8 +21,25 @@ class App extends React.Component{
 
     this.setState({
       isLoading: false,
-      data: data
+      data: _.orderBy(data, this.state.sortField, this.state.sort)
     })
+  }
+
+  onSort = sortField => {
+    const clonedData = this.state.data.concat()
+    const sortType = this.state.sort === 'asc' ? 'desc' : 'asc'
+
+    const orderedData = _.orderBy(clonedData, sortField, sortType)
+
+    this.setState({
+      data: orderedData,
+      sort: sortType,
+      sortField: sortField
+    })
+  }
+
+  onRowSelect = row => {
+    this.setState({row})
   }
 
   render() {
@@ -26,7 +49,18 @@ class App extends React.Component{
           this.state.isLoading
             ? <Loader />
             : <Table
-                data={this.state.data}/>
+                data={this.state.data}
+                onSort={this.onSort}
+                sort={this.state.sort}
+                sortField={this.state.sortField}
+                onRowSelect={this.onRowSelect}
+            />
+        }
+
+        {
+          this.state.row
+            ? <DetailRowView person={this.state.row} />
+            : null
         }
       </div>
     );
---
+++ src/components/DetailRowView/DetailRowView.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default ({person}) => (
+  <div>
+    <p>Выбран пользователь: <b>{person.firstName + ' ' + person.lastName}</b></p>
+    <p>
+      Описание: <br/>
+      <textarea defaultValue={person.description} cols="40" rows="6" />
+    </p>
+    <p>Индекс: <b>{person.address.zip}</b></p>
+    <p>Провинция/штат: <b>{person.address.state}</b></p>
+    <p>Город: <b>{person.address.city}</b></p>
+    <p>Адрес проживания: <b>{person.address.streetAddress}</b></p>
+  </div>
+)
